5ea0598cc93be21dd0b671d467d580dca29c817c
dsikarwar-ops <dsikarwar@bestpeers.com>
2025-10-13 17:03:40 +0530

    Update Home.jsx

--- Batch-1-2025/React/Class4-react-router-sideEffects/src/Pages/Home.jsx
+++ Batch-1-2025/React/Class4-react-router-sideEffects/src/Pages/Home.jsx
@@ -1,11 +1,18 @@
+import { useContext } from 'react'
 import { result } from '../../data.json'
 import { Card } from '../components/Card'
-
+import { ThemeContext } from '../context'
 
 export const Home = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return (
-        <div className='page'>
-            {result.map(item => <Card {...item} />)}
-        </div>
+        <>
+            <button onClick={toggleTheme}>Toggle theme: {theme}</button>
+
+            <div style={{ backgroundColor: theme }} className='page'>
+                {result.map(item => <Card {...item} />)}
+            </div>
+        </>
     )
 }
