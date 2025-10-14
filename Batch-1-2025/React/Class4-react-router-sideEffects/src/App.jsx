717e8a66ca5962f8c42f9472a4ba4657c101c70f
dsikarwar-ops <dsikarwar@bestpeers.com>
2025-10-14 13:27:30 +0530

    Update App.jsx

--- Batch-1-2025/React/Class4-react-router-sideEffects/src/App.jsx
+++ Batch-1-2025/React/Class4-react-router-sideEffects/src/App.jsx
@@ -3,24 +3,36 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { Product } from './Pages/Product';
 import { HomeV2 } from './Pages/HomeV2';
+import { useState } from 'react';
+import { ThemeContext } from './context';
+
 
 function App() {
 
-  return <BrowserRouter>
-    {/* Navigation */}
-    <nav className='navbar'>
-      {/* a tag will reload the application */}
-      {/* <a href='/'>Home</a> */}
-      <Link to="/">Home</Link>
-      <Link to="/v2">HomeV2</Link>
-    </nav>
+  const [theme, setTheme] = useState("white")
 
-    {/* Routes */}
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/v2" element={<HomeV2 />} />
-      <Route path="/product/:product_id" element={<Product />} />
-    </Routes>
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === "white" ? 'grey' : "white")
+  }
+
+  return <BrowserRouter>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      {/* Navigation */}
+      <nav className='navbar'>
+        {/* a tag will reload the application */}
+        {/* <a href='/'>Home</a> */}
+        <Link to="/">Home</Link>
+        <Link to="/v2">HomeV2</Link>
+        <button onClick={toggleTheme}>Theme: {theme}</button>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/v2" element={<HomeV2 />} />
+        <Route path="/product/:product_id" element={<Product />} />
+      </Routes>
+    </ThemeContext.Provider>
   </BrowserRouter>
 }
 
